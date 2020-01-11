7a15d5b88ad08728ebe00b2037327d5aa3e93b80
jerrylau <tenstone@foxmail.com>
2020-01-11 17:06:34 +0800

    gpu cpu

--- packages/tensorflow/mnist-cpu.py
+++ packages/tensorflow/mnist-cpu.py
@@ -4,8 +4,10 @@ import time
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+start = time.time()
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
@@ -14,7 +16,6 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(10, activation='softmax')
 ])
 
-start = time.time()
 
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
